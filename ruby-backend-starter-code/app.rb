209020cb7c735d4fb343506362252564de6bd0c9
rachelslurs <rachel.cantor@controlgroup.com>
2015-08-04 23:43:44 -0400

    fixing errors in starter ruby code

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,18 +1,23 @@
 require 'sinatra'
+require 'json' # Need to include the json helper
+require 'shotgun' # Not needed in production
 
-get '/'
-  File.read('index.html')
+get '/' do # Missing do
+  File.read('views/index.html') # Need to include the directory
 end
 
-get 'favorites' do
+#get '/search' do # for reaching omdbapi
+
+get '/favorites' do # Missing '/' to designate appropriate route
   response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
+  File.read('data.json') rescue [] # Handle if file is empty
 end
 
-get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
+post '/favorites' do # This needs to post to favorites
+  file = JSON.parse(File.read('data.json')) rescue [] # Handle if file is empty
   unless params[:name] && params[:oid]
     return 'Invalid Request'
+  end # remember to denote 'end' of 'unless'
   movie = { name: params[:name], oid: params[:oid] }
   file << movie
   File.write('data.json',JSON.pretty_generate(file))
